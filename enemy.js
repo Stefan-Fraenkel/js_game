3d6ff5ecdbf897d7a86f9d826ead3b12f9186545
gitlab-ci <stefan.fraenkel@gmail.com>
2023-02-11 16:42:02 +0100

    Update enemy.js
    fix bug allowing enemy to escape viewport

--- enemy.js
+++ enemy.js
@@ -28,29 +28,43 @@
 
      move(current_pos, view_width_height)
      {
+         this.coordinates = document.getElementById(this.id).getBoundingClientRect()
          let animationRate = 20;
          if (this.timer >= animationRate) {
              document.getElementById(this.id).style.backgroundImage =  "url('/animation/ghost.png')";
              this.timer = 0;
          }
          this.timer++;
+
          if (typeof this.destination_x === 'undefined' || typeof this.destination_y === 'undefined') {
-             this.destination_x = this.getRndInteger(0, view_width_height[0]+1); // Returns a random integer from 0 to w
-             this.destination_y = this.getRndInteger(0, view_width_height[1]+1); // Returns a random integer from 10 to h
+             this.newDestination(view_width_height);
              this.rotation_enemy = Math.atan2(this.destination_y - current_pos.top, this.destination_x - current_pos.left);
          }
 
          while (current_pos.left <= this.destination_x && current_pos.left + current_pos.width >= this.destination_x && current_pos.top <= this.destination_y && current_pos.top + current_pos.height >= this.destination_y) {
-             this.destination_x = this.getRndInteger(0, view_width_height[0]+1);
-             this.destination_y = this.getRndInteger(0, view_width_height[1]+1);
+             this.newDestination(view_width_height);
              this.rotation_enemy = Math.atan2(this.destination_y - current_pos.top, this.destination_x - current_pos.left);
          }
-         this.coordinates = document.getElementById(this.id).getBoundingClientRect()
+
          var velocity = 2;
          var enemy_x = Math.cos(this.rotation_enemy) * velocity;
          var enemy_y = Math.sin(this.rotation_enemy) * velocity;
 
+         while ((current_pos.left + enemy_x) < 0 || (current_pos.left + enemy_x) > view_width_height[0] || (current_pos.top + enemy_y) < 0 || (current_pos.top + enemy_y) > view_width_height[1])
+         {
+             this.newDestination(view_width_height);
+             this.rotation_enemy = Math.atan2(this.destination_y - current_pos.top, this.destination_x - current_pos.left);
+             enemy_x = Math.cos(this.rotation_enemy) * velocity;
+             enemy_y = Math.sin(this.rotation_enemy) * velocity;
+         }
+
          return [current_pos.left + enemy_x + "px", current_pos.top + enemy_y + "px"];
+     }
+
+     newDestination(view_width_height)
+     {
+         this.destination_x = this.getRndInteger(0, view_width_height[0]+1); // Returns a random integer from 0 to w
+         this.destination_y = this.getRndInteger(0, view_width_height[1]+1); // Returns a random integer from 0 to h
      }
 
      hit(bullet)
